2d39f3da92190b9fe8359938010a01b14429721a
james <jamesdejesus74@gmail.com>
2025-05-27 20:34:00 +0800

    init refactor

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -7,40 +7,40 @@ import dhe from "../assets/dhe.png";
 import dhm from "../assets/dhm.png";
 
 const Hero = () => {
-  return (
-    <>
-      <section className="hero">
-        <HeroCard
-          title="DISNEY+, HULU BUNDLE BASIC"
-          imageSrc={dh}
-          description="Both with ads, for $10.99/month."
-          buttonText="GET THEM BOTH"
-        />
+	return (
+		<>
+			<section className="hero">
+				<HeroCard
+					title="DISNEY+, HULU BUNDLE BASIC"
+					imageSrc={dh}
+					description="Both with ads, for $10.99/month."
+					buttonText="GET THEM BOTH"
+				/>
 
-        <HeroCard
-          title="DISNEY+, HULU, MAX BUNDLE"
-          imageSrc={dhm}
-          description="Plans starting at $16.99/month."
-          buttonText="LEARN MORE"
-        />
-      </section>
+				<HeroCard
+					title="DISNEY+, HULU, MAX BUNDLE"
+					imageSrc={dhm}
+					description="Plans starting at $16.99/month."
+					buttonText="LEARN MORE"
+				/>
+			</section>
 
-      <section className="mid-hero">
-        <HeroCard
-          title="DISNEY+, HULU, ESPN+, BUNDLE BASIC"
-          imageSrc={dhe}
-          description="Get Hulu, Disney+, and ESPN+, all with ads, for $16.99/mo."
-          buttonText="GET ALL THREE"
-          orientation="horizontal"
-        />
-      </section>
+			<section className="mid-hero">
+				<HeroCard
+					title="DISNEY+, HULU, ESPN+, BUNDLE BASIC"
+					imageSrc={dhe}
+					description="Get Hulu, Disney+, and ESPN+, all with ads, for $16.99/mo."
+					buttonText="GET ALL THREE"
+					orientation="horizontal"
+				/>
+			</section>
 
-      <section className="bot-hero">
-        <p>BUILD THE PLAN THAT'S RIGHT FOR YOU</p>
-        <Buttons text="START BUILDING" variant="bot" />
-      </section>
-    </>
-  );
+			<section className="bot-hero">
+				<p>BUILD THE PLAN THAT'S RIGHT FOR YOU</p>
+				<Buttons text="START BUILDING" variant="bot" />
+			</section>
+		</>
+	);
 };
 
 export default Hero;
